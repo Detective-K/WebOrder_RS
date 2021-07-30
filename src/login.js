dd7097cf4240549fd5f6aa545bb56186d6f0f367
Detective-K <asfako1122@gmail.com>
2021-07-30 17:28:34 +0800

    2021.7.30

--- src/login.js
+++ src/login.js
@@ -1,24 +1,45 @@
 //import App from './App';
 
 
-class App extends React.Component{
+class App extends React.Component {
 
-    render(){
-        return(
-                    <div className="wrapper fadeInDown">
-                        <div id="formContent">
+    constructor(props) {
+        super(props);
+        this.state = { isToggleOn: true };
+
+        // 為了讓 `this` 能在 callback 中被使用，這裡的綁定是必要的：
+        this.loginClick = this.loginClick.bind(this);
+    }
+
+    loginClick() {
+        fetch("http://192.168.0.218/APEX-API/api/order/noauth", {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(function (res) {
+            console.log(res);
+        })
+            .catch(error => console.error('Error:', error))
+    }
+
+    render() {
+        return (
+            <div className="wrapper fadeInDown">
+                <div id="formContent">
 
                     <div className="fadeIn first">
 
-                             <a className="navbar-brand top-navbar-brand" href="#"><img src="http://192.168.150.33/AttendanceSystem/public/images/logo2.png"/> APEX</a>
+                        <a className="navbar-brand top-navbar-brand" href="#"><img src="http://192.168.150.33/AttendanceSystem/public/images/logo2.png" /> APEX</a>
                     </div>
 
 
                     <form>
 
-                      <input type="text" id="login" className="fadeIn second" name="login" placeholder="login" />
-                      <input type="text" id="password" className="fadeIn third" name="login" placeholder="password"/>
-                      <input type="submit" formaction="./index.html" className="fadeIn fourth " value="Log In" />
+                        <input type="text" id="login" className="fadeIn second" name="login" placeholder="login" />
+                        <input type="text" id="password" className="fadeIn third" name="login" placeholder="password" />
+                        <input type="submit" formaction="./index.html" className="fadeIn fourth " value="Log In" />
+                        <input onClick={this.loginClick} className="fadeIn fourth " value="Log In2" />
                     </form>
 
 
@@ -26,7 +47,7 @@ class App extends React.Component{
 
                     </div>
 
-              </div>
+                </div>
             </div>
         );
     }
